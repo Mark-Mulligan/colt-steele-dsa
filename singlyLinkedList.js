d13b3c933f8ed8678d8fecb732c11b8311d27862
Mark-Mulligan <mark.mulligan.jr1@gmail.com>
2022-10-21 17:14:17 -0500

    singlyLinkedList

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -75,8 +75,63 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+
+    return current;
+  }
+
+  set(index, val) {
+    const targetNode = this.get(index);
+
+    if (targetNode) {
+      targetNode.val = val;
+      return true;
+    }
+
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unShift(val);
+
+    let previousNode = this.get(index - 1);
+    let newNode = new Node(val);
+    newNode.next = previousNode.next;
+    previousNode.next = newNode;
+
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+
+    let previousNode = this.get(index - 1);
+    let nextNode = this.get(index + 1);
+    previousNode.next = nextNode;
+    this.length--;
+  }
 }
 
 const list = new SinglyLinkedList();
 
-console.log(list.unShift(1));
+list.push(1);
+list.push(3);
+list.push(4);
+
+console.log(list);
+console.log(list.insert(1, 2));
+console.log(list);
