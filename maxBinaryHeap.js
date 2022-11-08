95ea8a04c0f6fb39bc8c8e672073ad460d0194c4
Mark-Mulligan <mark.mulligan.jr1@gmail.com>
2022-11-08 09:11:23 -0600

    added max binary heap extract map

--- maxBinaryHeap.js
+++ maxBinaryHeap.js
@@ -3,49 +3,73 @@ class MaxBinaryHeap {
     this.values = [];
   }
 
-  insert(val) {
-    this.values.push(val);
-    let currentIndex = this.values.length - 1;
-    let parentIndex = Math.floor((currentIndex - 1) / 2);
-    let temp;
-
-    while (currentIndex !== 0 && this.values[currentIndex] > this.values[parentIndex]) {
-      temp = this.values[parentIndex];
-      this.values[parentIndex] = this.values[currentIndex];
-      this.values[currentIndex] = temp;
-
-      currentIndex = parentIndex;
-      parentIndex = Math.floor((currentIndex - 1) / 2);
-    }
-
-    return this.values;
-  }
-
-  bubbleUp() {
-    let index = this.values.length - 1;
-    const element = this.values[index];
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      let parent = this.values[parentIndex];
-      if (element <= parent) break;
-      this.values[parentIndex] = element;
-      this.values[index] = parent;
-      index = parentIndex;
-    }
-  }
-
-  insert2(element) {
+  insert(element) {
     this.values.push(element);
     this.bubbleUp();
-    return this.values;
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
   }
 }
 
 const heap = new MaxBinaryHeap();
+heap.insert2(41);
+heap.insert2(39);
+heap.insert2(33);
 heap.insert2(18);
 heap.insert2(27);
-heap.insert2(39);
 heap.insert2(12);
-heap.insert2(33);
-heap.insert2(11);
-console.log(heap.insert2(50));
+heap.insert2(55);
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
