8fbc04f042044a561f49db13e3251ab54eb526bd
Mark-Mulligan <mark.mulligan.jr1@gmail.com>
2022-11-11 10:27:09 -0600

    added more graph methods

--- graph.js
+++ graph.js
@@ -18,13 +18,31 @@ class Graph {
     this.adjacencyList[v1] = this.adjacencyList[v1].filter((edge) => edge !== v2);
     this.adjacencyList[v2] = this.adjacencyList[v2].filter((edge) => edge !== v1);
   }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(adjacentVertex, vertex);
+    }
+
+    delete this.adjacencyList[vertex];
+  }
 }
 
 const graph = new Graph();
 graph.addVertex("Tokyo");
 graph.addVertex("Dallas");
 graph.addVertex("Aspen");
+graph.addVertex("Hong Kong");
+graph.addVertex("Los Angeles");
 graph.addEdge("Tokyo", "Dallas");
+graph.addEdge("Dallas", "Aspen");
+graph.addEdge("Hong Kong", "Tokyo");
+graph.addEdge("Dallas", "Hong Kong");
+graph.addEdge("Dallas", "Los Angeles");
+graph.addEdge("Hong Kong", "Los Angeles");
 console.log(graph);
-graph.removeEdge("Tokyo", "Dallas");
+graph.removeVertex("Hong Kong");
 console.log(graph);
+// graph.removeEdge("Tokyo", "Dallas");
+// console.log(graph);
